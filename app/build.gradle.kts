a97e05236a5b62c8f41a66e7ace3248b02a90335
vtn12052 <ngocviet12052@gmail.com>
2026-08-05 21:22:15 +0700

    Chore: Prepare v1.0.0-beta.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0-beta.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
