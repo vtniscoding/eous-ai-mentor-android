320a22f27cf002a934223559ea67309b65537a69
vtn12052 <ngocviet12052@gmail.com>
2026-08-06 18:58:31 +0700

    Build: Bump version to v1.0.0-beta.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.eous.mentor"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0-beta.1"
+        versionCode = 2
+        versionName = "1.0.0-beta.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
